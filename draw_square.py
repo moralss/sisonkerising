d184a936ab39ea6a79e9d263da81cb0283940ada
moralss <jeramoral@gmail.com>
2016-05-13 11:10:54 -0400

    drawed a square

---
+++ draw_square.py
@@ -0,0 +1,10 @@
+def draw_square(length_size,square_color):
+    color(square_color)
+    go(length_size)
+    turn(90)
+    go(length_size)
+    turn(90)
+    go(length_size)
+    turn(90)
+    go(length_size)
+	
