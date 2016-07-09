56e8a58a27dc94a703bd1ba45a98b64f41e11e5e
moralss <jeramoral@gmail.com>
2016-07-09 09:30:07 -0400

    made changes

--- python_collection/my_dictionary_cars.py
+++ python_collection/my_dictionary_cars.py
@@ -1,4 +1,6 @@
 favourite_cars = {1:'benz',2:'audi',3:'golf'}
-for car in favourite_cars:
-    print("%s %s" %(car , favourite_cars[car]))
+def display_cars():
+    for car in favourite_cars:
+        print("%s %s" %(car , favourite_cars[car]))
 
+display_cars()
