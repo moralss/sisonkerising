90742a2ce436e63449de5099d75525131b05af9b
moralss <jeramoral@gmail.com>
2016-07-13 09:44:56 -0400

    made changes

--- python_collection/my_dictionary_cars.py
+++ python_collection/my_dictionary_cars.py
@@ -1,6 +1,6 @@
 favourite_cars = {1:'benz',2:'audi',3:'golf'}
 
-#It print underneath each other 
+# prints underneath each other 
 def display_cars():
     for position in favourite_cars:
         print("%s %s" %(position , favourite_cars[position]))
