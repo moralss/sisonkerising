55f604eb89f4662a123ab23993bc1d1a4a01c5bf
moralss <jeramoral@gmail.com>
2016-07-30 07:11:13 -0400

    moral

---
+++ adding_buttons/board.py
@@ -0,0 +1,52 @@
+from tkinter import*
+from tkinter import ttk
+root = Tk()
+
+canvas = Canvas(root,width=800,height=600)
+
+
+favourite_cars = {"bmw":"2015audis8plusplacement626x382.gif","audi":"images.gif"}
+
+
+def show_picture_for_bmw():
+    global photo
+    photo = PhotoImage(file = "images.gif")
+    label = Label(root, image = photo)
+    label.pack()
+    
+def show_picture_for_audi():
+    global photo
+    photo = PhotoImage(file = "2015audis8plusplacement626x382.gif")
+    label = Label(root, image = photo)
+    label.pack()
+
+def show_picture_for_golf():
+    global photo
+    photo = PhotoImage(file = "maxresdefault.gif")
+    label = Label(root, image = photo)
+    label.pack()
+    
+
+
+def create_button_for_bmw():
+    Button = ttk.Button(text = "bmw" , command=lambda:show_picture_for_bmw())
+    Button.pack()
+
+def create_button_for_audi():
+    Button = ttk.Button(text = "audi" , command=lambda:show_picture_for_audi())
+    Button.pack()
+
+def create_button_for_golf():
+    Button = ttk.Button(text = "golf" , command=lambda:show_picture_for_golf())
+    Button.pack()
+
+    
+    
+    
+        
+
+create_button_for_bmw()
+create_button_for_audi()
+create_button_for_golf()
+root.mainloop()
+
---
+++ python_collection/example.py
@@ -0,0 +1,11 @@
+
+
+soccer_teams = {2:"manchester united",1:"ajax",3:"madrid"}
+def display_soccer_team():
+    for team in soccer_teams:
+        print("%s %s" % (team , soccer_teams[team])) 
+
+display_soccer_team()
+
+    
+    
