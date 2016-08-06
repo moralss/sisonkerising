846ec55d778432ab2731152e1aaf2c8c1cc05e27
moralss <jeramoral@gmail.com>
2016-08-06 19:28:35 -0400

    ade changes

---
+++ bubble_sorting/bubble_sorting.py
@@ -0,0 +1,21 @@
+
+def bubble_sorting(list_ages,total_number_of_ages):
+    if total_number_of_ages == 0:
+        print(list_ages)
+        return
+
+    if (len(list_ages) == 1):
+        print(list_ages)
+        return
+        
+    else:
+        for index in range(total_number_of_ages):
+            if(list_ages[index] > list_ages[index+1]):
+                list_ages[index],list_ages[index+1] = list_ages[index+1],list_ages[index]
+            
+        bubble_sorting(list_ages,total_number_of_ages-1)
+
+list_ages=[8,4,5,18,0]
+
+bubble_sorting(list_ages,len(list_ages)-1)
+        
