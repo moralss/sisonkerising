3b9d1a5a6f50969d16cc215ec6f98af3f829467e
moralss <jeramoral@gmail.com>
2016-08-07 10:18:47 -0400

    ade changes

--- bubble_sorting/bubble_sorting_excise.py
+++ bubble_sorting/bubble_sorting_excise.py
@@ -1,6 +1,15 @@
 list_of_ages = [8,4,5,18,0]
 
+def print_backwards(list_of_ages):
+    new = []
+    count = len(list_of_ages) -1
+    for index in range(0,len(list_of_ages)):
+        new.append(list_of_ages[count])
+        count -=1
+        print(new) 
 
+
+print_backwards(list_of_ages)
 
 def bubble_sort(list_of_ages):
     for index in range(0,len(list_of_ages)):
