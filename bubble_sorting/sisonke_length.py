06c832fde476c51c07a9467153f1effc876b21fa
moralss <jeramoral@gmail.com>
2016-08-13 08:46:22 -0400

    ade changes

---
+++ bubble_sorting/sisonke_length.py
@@ -0,0 +1,25 @@
+my_list_1 = [1,5,8,11,12,15]
+def get_value_at( value , my_list):
+    for index in my_list:
+        show = my_list[value]
+    print(my_list[value])
+
+#get_value_at(2,my_list_1)
+    
+def get_index_of_value(value,my_list):
+    for index in range(0,len(my_list)):
+        show = my_list[value]
+    print(show)
+
+##get_index_of_value(1,my_list_1)
+
+
+def sisonke_length(my_list):
+    for index in my_list:
+        value = range(0,index+1)
+    for total in value:
+        number = value    
+    print(int(number)
+
+sisonke_length(my_list_1)
+
